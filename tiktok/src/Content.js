71696962f818efcc9ed58655595e923c68e96b18
PhucPoo <hoangphuc1161998@gmail.com>
2025-10-28 08:18:39 +0700

    useLayoutEffect() hook

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -1,84 +1,38 @@
-import { use, useEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 
 
-// Event: Add / Remove event Listener
-// Observer pattern: Subscribe/Unsubscribe
-// Closure
-// Timers: setInterval, setTimeout, clearInterval, clearTimeout
-// useState
-// Mounted/Unmounted
-// Call API
+// useEffect
+// 1. Cập nhật lại state 
+// 2. Cập nhật lại DOM (mutated) 
+// 3. Render lại UI 
+// 4. Gọi cleanup nếu deps thay đổi 
+// 5. Gọi useEffect callback 
 
-/***
- * 1.Update DOM
- * _F8 blog title
- * 2. Call API
- * 3.Listen DOM events
- * _Scroll
- * _Resize
- * 4. Cleanup
- * _Remove listeners/ unsubscribe
- * _Clear timers  
- */
-//1. useEffect(callback)
-// _Gọi call back mỗi khi component re-render
-// _ Gọi callback mỗi khi component thêm element vào DOM
-// 2. useEffect(callback, [])
-// _ Chỉ gọi callback 1 lần sau khi component mounted
-// 3. useEffect(callback, [deps])
-// _ Callback được gọi mỗi khi deps thay đổi
+// useLayoutEffect 
+// 1. Cập nhật lại state
+// 2. Cập nhật lại DOM (mutated) 
+// 3. Gọi cleanup nếu deps thay đổi (sync) 
+// 4. Gọi useLayoutEffect callback (sync)
+// 5. Render lại UI
 
-// 1.callback luôn được gọi sau khi component mounted
-// 2. Cleanup function luôn được gọi trước khi unmounted
-// 3. Cleanup function luôn được gọi trước khi callback được gọi ()
-
-
-const lessons =[
-    {
-        id:1,
-        name: 'ReactJS là gì ...',
-    },
-    {
-        id:2 ,
-        name: 'Spa/MPA là gì?'
-    },
-    {
-        id :3,
-        name: 'Arrow function' 
-    }
-]
 function Content() {
+    const [count, setCount] = useState(0)
 
-    const [lessonId, setLessonId] = useState(1)
+    useLayoutEffect(()=> {
+        if(count > 3){
+            setCount(0)
+        }
+    },[count])
 
-   useEffect(()=>{
-    const handleComment = ({detail}) => {
-        console.log(detail);
-        
+    const handleRun =() => {
+        setCount(count + 1)
     }
-    window.addEventListener(`lesson-${lessonId}`,handleComment)
-
-    return ()=>{
-        window.removeEventListener(`lesson-${lessonId}`,handleComment)
-    }
-
-   },[lessonId])
+ 
 
     return (
         <div>
-           <ul>
-            {lessons.map(lesson => (
-                <li
-                    key={lesson.id}
-                    style={{
-                        color: lessonId===lesson.id ? 'red ':'#333'
-                    }}
-                    onClick={()=>setLessonId(lesson.id)}
-                >
-                    {lesson.name}
-                </li>
-            ))}
-           </ul>
+          <h1>{count}</h1>
+          <button onClick ={handleRun}>Run</button>
 
         </div>
     );
